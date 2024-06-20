88b4efdbfac874ba8f9eae84a2505431e145f75a
Juan Colapaolo <145474462+jigcolapaolo@users.noreply.github.com>
2024-06-20 19:42:56 -0300

    optimize logic

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ const output = document.getElementById("output");
 
 //Convert Data
 const romanArabic = [
-    { roman: "M", arabic: 1000, digits: 4 },
-    { roman: "CM", arabic: 900, digits: 3 },
-    { roman: "D", arabic: 500, digits: 3 },
-    { roman: "CD", arabic: 400, digits: 3 },
-    { roman: "C", arabic: 100, digits: 3 },
-    { roman: "XC", arabic: 90, digits: 2 },
-    { roman: "L", arabic: 50, digits: 2 },
-    { roman: "XL", arabic: 40, digits: 2 },
-    { roman: "X", arabic: 10, digits: 2 },
-    { roman: "IX", arabic: 9, digits: 1 },
-    { roman: "V", arabic: 5, digits: 1 },
-    { roman: "IV", arabic: 4, digits: 1 },
-    { roman: "I", arabic: 1, digits: 1 }
+    { roman: "M", arabic: 1000 },
+    { roman: "CM", arabic: 900 },
+    { roman: "D", arabic: 500 },
+    { roman: "CD", arabic: 400 },
+    { roman: "C", arabic: 100 },
+    { roman: "XC", arabic: 90 },
+    { roman: "L", arabic: 50 },
+    { roman: "XL", arabic: 40 },
+    { roman: "X", arabic: 10 },
+    { roman: "IX", arabic: 9 },
+    { roman: "V", arabic: 5 },
+    { roman: "IV", arabic: 4 },
+    { roman: "I", arabic: 1 }
 ];
 
 
@@ -46,53 +46,14 @@ const validateInput = (input) => {
 
 //Logic
 const convertToRoman = (number) => {
-  let numArr = number.split("").reverse();
-  let result = "";
-
-  numArr.forEach((number, index) => {
-
-    const filterData = romanArabic.filter((data) => data.digits === (index + 1));
-
-    let numberDigits = parseInt(number += "0".repeat(index));
-    let checkAddOutput = false;
-    filterData.forEach((data) => {
-
-      if(data.arabic === numberDigits){
-                result += data.roman.split("").length > 1 ? data.roman.split("").reverse().join("") : data.roman;
-                checkAddOutput = true;
-      }
-
-    });
-
-    let rep = 0;
-    let fixResult = "";
-
-
-if(!checkAddOutput){
-
-    if(numberDigits.toString().split("").length === 4){
-
-      rep = numberDigits.toString().split("")[0] - filterData[0].arabic.toString().split("")[0];
-      fixResult = filterData[0].roman + filterData[0].roman.repeat(rep);
-      result += fixResult.split("").reverse().join("");
-    
-    } else if(numberDigits < filterData[0].arabic && numberDigits > filterData[1].arabic){
-
-      rep = numberDigits.toString().split("")[0] - filterData[1].arabic.toString().split("")[0];
-      fixResult = filterData[1].roman + filterData[3].roman.repeat(rep);
-      result += fixResult.split("").reverse().join("");
-
-    } else if (numberDigits < filterData[2].arabic && numberDigits > filterData[3].arabic) {
-
-      rep = numberDigits.toString().split("")[0] - filterData[3].arabic.toString().split("")[0];
-      fixResult = filterData[3].roman + filterData[3].roman.repeat(rep);
-      result += fixResult.split("").reverse().join("");
-
+  let romanNumber = "";
+  
+  romanArabic.forEach(item => {
+    while(number >= item.arabic){
+      romanNumber += item.roman;
+      number -= item.arabic;
     }
-}
   });
 
-
-  return result.split("").reverse().join("");
+  return romanNumber;
 };
-
